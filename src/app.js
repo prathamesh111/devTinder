1b2d6151bd46e0de92197e4a796166a86a516202
prathamesh111 <pratham.samudra@gmail.com>
2025-10-25 15:36:50 +0530

    erorr handling

--- src/app.js
+++ src/app.js
@@ -2,47 +2,17 @@ const express = require('express');
 
 
 const app = express();
-const {adminAuth} = require('./middleewares/auth');
 
-// app.get('/user/:userId',(req, res) => {
-//     console.log(req.params.userId)
-//     res.send('testing page get');
-// });
+app.get("/admin", (req, res)=>{
+    throw new Error('Server error in admin route');
+});
 
+app.use("/",(err,req, res, next) => {
+    if(err){
+        // console.error(err);
+        res.status(502).send('Some error occured at server');
+    }
 
-// app.post("/user",(req, res) => {
-//      res.send('testing page post');
-// });
-
-
-// app.delete("/user",(req, res) => {
-//     res.send('testing page delete');
-// });
-
-// app.patch("/user",(req, res) => {
-//     res.send('testing page patch');
-// })
-
-// app.use('/test', (req, res, next)=>{
-//     console.log("middleware called");   
-//     // res.send('testing middleware');
-//     next();
-// }, (req, res, next)=>{
-//     console.log("second middleware called");
-//     next();
-// },(req, res, next)=>{
-//     console.log("second middleware called");
-//     next();
-// },(req, res, next)=>{
-//     res.send("final middleware called");
-//     next();
-// })
-
-
-// app.use('/admin',adminAuth);
-
-app.get('/admin/getAllUsers',adminAuth, (req, res) => {
-    res.send('list of all users');
 });
 
 app.listen(7777, ()=>{
