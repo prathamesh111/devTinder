c8e167d8cadb68bfb2615a88cde272350302b2a6
prathamesh samudra <prathamsamudra@gmail.com>
2025-12-17 20:35:12 +0530

    Update express dependency version and modify database connection string

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDb  = async() => {
-    await mongoose.connect('mongodb+srv://prathamsamudra_db_user:C11m6Wx7FsApojyz@namastenode.nxgzvwx.mongodb.net/devTinder');
+    await mongoose.connect('mongodb+srv://prathamsamudra_db_user:Express%402026@namastenode.nxgzvwx.mongodb.net/');
 }
 
 
